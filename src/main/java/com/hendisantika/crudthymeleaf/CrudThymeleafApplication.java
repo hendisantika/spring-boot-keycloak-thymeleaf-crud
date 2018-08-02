e0962901990c53a483474b999da0c41e37cd8776
Hendi Santika <hendisantika@gmail.com>
2018-08-02 07:11:15 +0700

    Initial commit

---
+++ src/main/java/com/hendisantika/crudthymeleaf/CrudThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.crudthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudThymeleafApplication.class, args);
+    }
+}
